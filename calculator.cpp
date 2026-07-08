bf2fa8f740c7599ef326ef9e97bdbb84278742e4
Pranjal1500 <swatiuphade8@gmail.com>
2026-07-08 22:05:06 +0530

    added one more way to do calculations and that part is commented

--- calculator.cpp
+++ calculator.cpp
@@ -14,6 +14,27 @@ int main(){
     cout << "division = " << a / b << endl;
     cout << "Remainder = "<< a % b <<  endl;
 
+    // int a, b;
+    // char op;
+    // cout << "Enter a : ";
+    // cin >> a;
+    // cout << "Enter b : ";
+    // cin >> b;
+    // cout << "Enter operator to perform operations(+, -, * , /, %) : " ;
+    // cin >> op;
+    // switch(op){
+    //     case '+' : cout << "sum = " << a + b << endl;
+    //     break;
+    //     case '-' : cout << "diffference = " << a - b << endl;
+    //     break;
+    //     case '*' : cout << "Multiplication = " << a * b << endl;
+    //     break;
+    //     case '/' : cout << "division = " << a - b << endl;
+    //     break;
+    //     default : cout << "Invalid operator" << endl;
+
+    // }
+
 
     return 0;
 }
