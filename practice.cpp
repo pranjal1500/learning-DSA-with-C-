8369a5fc348c9acde0f6baa29e039d88578ba2f5
Pranjal1500 <swatiuphade8@gmail.com>
2026-07-08 16:38:13 +0530

    starting DSA with C++

---
+++ practice.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int age = 25;
+    // char grade = 'a';
+    // float PI = 3.14f;
+    // bool isSafe = false;
+    // double price = 100.99;
+    // cout << isSafe << endl;//true -> 1 & false -> 0
+    // cout << PI << endl;
+    // cout << grade << endl;
+    // cout << age << endl;
+
+    // char grade = 'a';
+
+    // int value = grade;
+    // cout << value << endl;
+    // double price;
+    // cout << "Enter the price : ";
+    // cin >> price ;
+
+    // cout << "you entered price = " << price << endl;
+
+    // int a=10,b=5;
+ 
+    // cout << "sum = " << (a+b) << endl;
+    // cout << "difference = " << (a-b) << endl;
+    // cout << "product = " << (a*b) << endl;
+    // cout << "division = " << (a/b) << endl;
+
+    int a = 5;
+    double b = 2;
+
+    cout << (a/b) << endl;
+    return 0;
+}
