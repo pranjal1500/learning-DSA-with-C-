2883bda5187df6450f74344d815ffe3bc6fe07d7
Pranjal1500 <swatiuphade8@gmail.com>
2026-07-17 17:50:52 +0530

    sum of numbers divisble by 3 from 1 to n

--- sumdiv3.cpp
+++ sumdiv3.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 int main(){
-    int n=15;
-    int sum = 0;
-    for(int i=1; i<=n ;i++){
-        if(i%3==0){
-            sum +=i;
-        }
+    // int n=15;
+    // int sum = 0;
+    // for(int i=1; i<=n ;i++){
+    //     if(i%3==0){
+    //         sum +=i;
+    //     }
+    // }
+    int n, sum = 0;
+    int i=1;
+    cout << "Enter n : ";
+    cin >> n;
+    while(i<=n){
+        sum += i;
+        i++;
     }
-
     cout << "sum = " << sum << endl;
     return 0;
 }
