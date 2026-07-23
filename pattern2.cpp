a22d48cae05e98d4d24e62c15dee9a5e0efb32d0
Pranjal1500 <swatiuphade8@gmail.com>
2026-07-23 19:27:39 +0530

    learned nested loops through patterns

---
+++ pattern2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+    for(int i=0; i<n; i++){
+        ch = 'A';
+        for(int j=0; j<n; j++){
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
